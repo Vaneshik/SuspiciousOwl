f36b99b1c605e35c9e181b65a2713e78d6bba9ac
Your Name <you@example.com>
2022-04-11 17:29:35 +1000

    idk

--- js/normcode.js
+++ js/normcode.js
@@ -48,11 +48,58 @@ async function main() {
     var mask = cv.Mat.zeros(height, width, cv.CV_8UC1)
     eye_on_mask(mask, points_left)
     eye_on_mask(mask, points_right)
-    cv.dilate(mask, mask, cv.Mat.ones(9, 9, cv.CV_8UC1), new cv.Point(5, 5))
-    var eyes = new cv.Mat()
-    cv.bitwise_and(img, img, eyes, mask)
+    // cv.dilate(mask, mask, cv.Mat.ones(9, 9, cv.CV_8UC1), new cv.Point(7, 7))
 
-    console.log(eyes.rows + " " + eyes.cols)
+    var eyes = new cv.Mat()
+    var mid = Math.floor((points_left[3][0] + points_left[0][0]) / 2)
+    cv.bitwise_and(img, img, eyes, mask)
+    black2white(eyes)
+    cv.cvtColor(eyes, eyes, cv.COLOR_BGR2GRAY)
+    var thresh = new cv.Mat()
+    let val = document.getElementById('trackbar').value
+    cv.threshold(eyes, thresh, parseInt(val), 255, cv.THRESH_BINARY)
+    process_thresh(thresh)
+    cv.imshow("canvasOutput", thresh)
+
+    // var qqq = thresh.clone().data
+    // contouring(thresh, mid, img)
+    // var eyeball_pos_left = contouring(thresh[:, 0:mid], mid, img, end_points_left)
+    // var eyeball_pos_right = contouring(thresh[:, mid:], mid, img, end_points_right, true)    
+}
+
+function contouring(thresh, mid, img) {
+    let contours = new cv.MatVector();
+    let hierarchy = new cv.Mat();
+    cv.findContours(thresh, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    // contours.sort((a, b) => cv.contourArea(a) < cv.contourArea(b) ? 1 : -1)
+    // var cnt = contours.get(0);
+    // console.log(cnt)
+    // M = cv.moments(cnt)
+    // cx = Math.floor(M.m10 / M.m00)
+    // cy = Math.floor(M.m01 / M.m00)
+    // if (right) {
+    //     cx += mid
+    // }
+    cv.imshow("canvasOutput", hierarchy)
+    // pos = find_eyeball_position(end_points, cx, cy)
+    // return pos
+}
+
+function find_eyeball_position(end_points, cx, cy) {
+    x_ratio = (end_points[0] - cx) / (cx - end_points[2])
+    y_ratio = (cy - end_points[1]) / (end_points[3] - cy)
+    if (x_ratio > 3) {
+        return 1
+    } else if (x_ratio < 0.33) {
+        return 2
+    } else if (y_ratio < 0.33) {
+        return 3
+    } else {
+        return 0
+    }
+}
+
+function black2white(eyes) {
     for (var row = 0; row < eyes.rows; row++) {
         for (var col = 0; col < eyes.cols; col++) {
             let pixel = eyes.ucharPtr(row, col);
@@ -63,20 +110,18 @@ async function main() {
             }
         }
     }
+}
 
-    cv.cvtColor(eyes, eyes, cv.COLOR_BGR2GRAY)
-    cv.threshold(eyes, eyes, 75, 255, cv.THRESH_BINARY)
-    cv.imshow("canvasOutput", eyes)
-}
 function process_thresh(thresh) {
-    cv2.erode(thresh, thresh, new cv.Mat.zeros(3, 3), cv.CV_8UC1, new cv.Point(-1, -1), 2)
-    cv2.dilate(thresh, thresh, new cv.Mat.zeros(3, 3), cv.CV_8UC1, new cv.Point(-1, -1), 4)
-    cv2.medianBlur(thresh, thresh, 3)
-    cv2.bitwise_not(thresh, thresh)
+    cv.erode(thresh, thresh, new cv.Mat.ones(3, 3, cv.CV_8UC1), new cv.Point(-1, -1), 2)
+    cv.dilate(thresh, thresh, new cv.Mat.ones(3, 3, cv.CV_8UC1), new cv.Point(-1, -1), 4)
+    cv.medianBlur(thresh, thresh, 3)
+    cv.bitwise_not(thresh, thresh)
 }
+
 window.addEventListener('load', async function (event) {
     await loadModels()
     runVideo()
-    setInterval(main, 500)
+    setInterval(main, 200)
 })
 
